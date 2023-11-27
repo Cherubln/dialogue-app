ed75679d8bcb31435ca62749b5d4b2f16f0d92b3
Cherubin Ndikubwayo <cherubinkubwayo@gmail.com>
2023-11-27 18:00:44 +0200

    fix error alert spacing

--- app/components/Auth.tsx
+++ app/components/Auth.tsx
@@ -123,7 +123,7 @@ function Register() {
           {error && (
             <div
               role="alert"
-              className="alert alert-error p-2 px-5 cursor-pointer"
+              className="alert alert-error p-2 px-5 cursor-pointer flex items-center"
               onClick={() => setError("")}
             >
               <svg
@@ -139,7 +139,7 @@ function Register() {
                   d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              <span className="text-base">{error}</span>
+              <span className="text-base flex-1">{error}</span>
             </div>
           )}
         </form>
